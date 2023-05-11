f644f503ada564c326bd0709102f6e9d2f0e150a
lilianiloani <chinweiloani@yahoo.com>
2023-05-12 00:33:25 +0200

    a commit

--- src/views/InscriptionView.vue
+++ src/views/InscriptionView.vue
@@ -1,3 +1,28 @@
-<template></template>
+<template>
+    <div>
+        <input type = "email" 
+        name = "email" 
+        v-model = "email"
+        placeholder = "email ici"
+        />
+        <br>
+        <br>
+
+        <input type = "password" 
+        name = "password" 
+        v-model = "password"
+        placeholder = "mot de passe  ici"
+        />
+        <br>
+        <br>
+        <button @click = "register">
+            Inscription
+        </button>
+    </div>
+
+
+
+
+</template>
 <script></script>
 <style></style>
